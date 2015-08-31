a5fd330d51d12d00e8dca7421972b0fde38b33e8
KSedlak <krzysztof.sedlak@hotmail.com>
2015-08-31 11:02:21 +0200

    add examples

--- spring-data-jpa/target/classes/import.sql
+++ spring-data-jpa/target/classes/import.sql
@@ -1,3 +1,7 @@
 insert into book (id, title, authors) values (1, 'Pierwsza książka', 'Jan Kowalski');
 insert into book (id, title, authors) values (2, 'Druga książka', 'Zbigniew Nowak');
 insert into book (id, title, authors) values (3, 'Trzecia książka', 'Janusz Jankowski');
+insert into book (id, title, authors) values (4, 'Potop', 'Henryk Sienkiewicz');
+insert into book (id, title, authors) values (5, 'W pustyni i w puszczy','Henryk Sienkiewicz');
+insert into book (id, title, authors) values (6, 'Pan Wolodyjowski','Henryk Sienkiewicz');
+insert into book (id, title, authors) values (7, 'Ogniem i mieczem','Henryk Sienkiewicz');
