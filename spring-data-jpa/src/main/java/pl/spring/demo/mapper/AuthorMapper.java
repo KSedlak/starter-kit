2907ed509a05737d7c9982964e2506dcb729583b
KSedlak <krzysztof.sedlak@hotmail.com>
2015-09-11 09:52:39 +0200

    add many to many AuthorEntity

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/mapper/AuthorMapper.java
@@ -0,0 +1,32 @@
+package pl.spring.demo.mapper;
+
+import pl.spring.demo.entity.AuthorEntity;
+import pl.spring.demo.to.AuthorTo;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AuthorMapper {
+
+    public static AuthorTo map(AuthorEntity AuthorEntity) {
+        if (AuthorEntity != null) {
+            return new AuthorTo(AuthorEntity.getId(), AuthorEntity.getFirstName(), AuthorEntity.getLastName());
+        }
+        return null;
+    }
+
+    public static AuthorEntity map(AuthorTo AuthorTo) {
+        if (AuthorTo != null) {
+            return new AuthorEntity(AuthorTo.getId(), AuthorTo.getFirstName(), AuthorTo.getLastName());
+        }
+        return null;
+    }
+
+    public static Set<AuthorTo> map2To(Set<AuthorEntity> AuthorEntities) {
+        return AuthorEntities.stream().map(AuthorMapper::map).collect(Collectors.toSet());
+    }
+
+    public static Set<AuthorEntity> map2Entity(Set<AuthorTo> AuthorEntities) {
+        return AuthorEntities.stream().map(AuthorMapper::map).collect(Collectors.toSet());
+    }
+}
--- spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
+++ spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
@@ -2,23 +2,32 @@ package pl.spring.demo.entity;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "BOOK")
 public class BookEntity implements Serializable {
-    @Id
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(nullable = false, length = 50)
     private String title;
-    @Column(nullable = false, length = 200)
-    private String authors;
-
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "BOOK_AUTHOR",
+            joinColumns = {@JoinColumn(name = "BOOK_ID", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "AUTHOR_ID", nullable = false, updatable = false)}
+    )
+    private Set<AuthorEntity> authors = new HashSet<>();
     // for hibernate
     protected BookEntity() {
 }
 
-    public BookEntity(Long id, String title, String authors) {
+    public BookEntity(Long id, String title, Set<AuthorEntity> authors) {
         this.id = id;
         this.title = title;
         this.authors = authors;
@@ -40,11 +49,13 @@ public class BookEntity implements Serializable {
         this.title = title;
     }
 
-    public String getAuthors() {
-        return authors;
-    }
+	public Set<AuthorEntity> getAuthors() {
+		return authors;
+	}
 
-    public void setAuthors(String authors) {
-        this.authors = authors;
-    }
+	public void setAuthors(Set<AuthorEntity> authors) {
+		this.authors = authors;
+	}
+
+ 
 }
