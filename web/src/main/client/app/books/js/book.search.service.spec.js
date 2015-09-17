1b060c203f2f17cda096deb240318fd244603615
KSedlak <krzysztof.sedlak@hotmail.com>
2015-09-17 09:56:30 +0200

    book.search.service.spec.js tests

--- web/src/main/client/app/books/js/book.search.service.spec.js
+++ web/src/main/client/app/books/js/book.search.service.spec.js
@@ -34,15 +34,56 @@ describe('book service', function() {
 	it('calls bookRestService.search', inject(function($q, bookRestService) {
 		// given
 		var searchDeferred = $q.defer();
+		var bookTitle='Lalka';
+		
+		var returnedBook={
+				data : [
+				        {id:1,
+				        title:bookTitle,
+				        authors:[{id:1,firstName:'Jan', lastName:'Kracy'}]
+				        }
+				        ]
+			};
 		spyOn(bookRestService, 'search').and
 				.returnValue(searchDeferred.promise);
 		// when
-		$bookService.search('Pierwsza');
-		searchDeferred.resolve({
-			data : null
-		});
+		$bookService.search(bookTitle);
+		searchDeferred.resolve(returnedBook);
 		// then
 		expect(bookRestService.search).toHaveBeenCalled();
+	}));
+	
+	it('calls bookRestService.save', inject(function($q, bookRestService) {
+		// given
+		var saveDeferred = $q.defer();
+		
+		var saveBook={
+				data : [
+				        {id:1,
+				        title:'Lalka',
+				        authors:[{id:1,firstName:'Jan', lastName:'Kracy'}]
+				        }
+				        ]
+			};
+		spyOn(bookRestService, 'saveBook').and
+				.returnValue(saveDeferred.promise);
+		// when
+		$bookService.saveBook(saveBook);
+		saveDeferred.resolve(saveBook);
+		// then
+		expect(bookRestService.saveBook).toHaveBeenCalled();
+	}));
+	
+	it('calls bookRestService.Delete', inject(function($q, bookRestService) {
+		// given
+		var deleteDeferred = $q.defer();
+		spyOn(bookRestService, 'deleteBook').and
+				.returnValue(deleteDeferred.promise);
+		// when
+		$bookService.deleteBook(1);
+		deleteDeferred.resolve();
+		// then
+		expect(bookRestService.deleteBook).toHaveBeenCalled();
 	}));
 	
 });
